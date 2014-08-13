0b43a4355c862372d307fdc623e21f2eae44b28a
Garth Whitten <garthwhitten@Garths-MacBook-Air.local>
2014-08-12 20:25:32 -0400

    Add comments and wrap opts in try block

--- books/books.py
+++ books/books.py
@@ -4,6 +4,8 @@ import os, sys, getopt
 
 
 def read(rootdir):
+    # Loops through rootdir opening and appending text by line
+    # Could use buffer if expecting larger files
     book_list = []
     for subdir, dirs, files in os.walk(rootdir):
         for file in files:
@@ -16,6 +18,8 @@ def read(rootdir):
 
 
 def parse(book_list):
+    # Parses through expected delimiters and order
+    # Returning a cleaned list of books
     clean_list = []
     for book in book_list:
         if "," in book:
@@ -46,64 +50,69 @@ def parse(book_list):
 
 
 def command_help():
+    # Returns a formatted  string
     return """Command to print out list of books.  Acceptable arguments:
         -h [or] --help:  Prints this text.
         --year:  Sorts by year in ascending order.
         --reverse:  Sorts by year in descending order.
         --filter [argument]:  Filters list by argument.
             ex.  python books --filter Agile
-        **** NOTE: arguments (other than help) are chainable ***
+        **** NOTE: --filter is chainable with --year or --reverse  ***
         """
 
 
 def command_year(clean_list, reverse=False):
+    # Sorts items by year with optional reverse flag
     new_list = sorted(clean_list, key=lambda k: k['Year'], reverse=reverse)
     return new_list
 
 
 def command_filter(clean_list, arg):
+    # Searches through list returning items
+    # that match query
     filtered_list = []
     for c in clean_list:
         for v in c.iteritems():
             if str(arg) in str(v[1]):
-                pruned_list.append(c)
+                filtered_list.append(c)
     return filtered_list
 
 
-def print_list(final_list):
+def format_list(final_list):
+    # Returns formatted list
     for f in final_list:
-        print (f["Last"] + ", "
+        return (f["Last"] + ", "
                + f["First"] + ", "
                + f["Title"] + ", "
                + str(f["Year"]))
 
 
 def main(argv):
+    # Main Function Call
+    # Returns list of books with optional sorting and filtering
     root = "src/"
     book_list = [b.rstrip() for b in read(root)]
     clean_list = parse(book_list)
 
-    opts, args = getopt.getopt(argv, 'h', ['help', 'filter=', 'year', 'reverse'])
+    try:
+        opts, args = getopt.getopt(argv, 'h', ['help', 'filter=',
+                                               'year', 'reverse'])
+        if opts:
+            for opt, arg in opts:
+                if opt in ("-h", "--help"):
+                    print command_help()
+                    return None
+                if opt == "--filter":
+                    clean_list = command_filter(clean_list, arg)
+                if opt == "--year":
+                    clean_list = command_year(clean_list)
+                elif opt == "--reverse":
+                    clean_list = command_year(clean_list, True)
+    except Exception as e:
+        print "Oops!  Encountered an error: {}".format(e)
 
-    if opts:
-        for opt, arg in opts:
-            if opt in ("-h", "--help"):
-                print command_help()
-                return None
-            if opt == "--filter":
-                clean_list = command_filter(clean_list, arg)
-            if opt == "--year":
-                clean_list = command_year(clean_list)
-            if opt == "--reverse":
-                clean_list = command_year(clean_list, True)
-
-    print_list(clean_list)
+    print format_list(clean_list)
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
-
-
-
-
